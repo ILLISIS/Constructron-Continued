4a07b3f14f987ba964a354514c526084103532f1
ILLISIS <92364928+ILLISIS@users.noreply.github.com>
2021-11-17 00:46:27 +1100

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend({
 		type = "int-setting",
 		name = "max-worker-per-job",
 		setting_type = "runtime-global",
-		default_value = 4,
+		default_value = 2,
 		minimum_value = 1,
         maximum_value = 10,
 	},
