d07cd6a9323bf07719f0b89a8431731cd10707ac
Nico <git@nicolas-lang.de>
2022-02-22 03:17:56 +0100

    Updated service-station sprite: now colored and with a bigger antenna

--- service_station.lua
+++ service_station.lua
@@ -1,8 +1,49 @@
+--  service_station images derived from 
+--    https://github.com/nEbul4/Factorio_Roboport_mk2/
+--    https://github.com/kirazy/classic-beacon/
 local service_station = table.deepcopy(data.raw["roboport"]["roboport"])
 service_station.name = "service_station"
-service_station.minable = {hardness = 0.2, mining_time = 0.5, result = "service_station"}
+service_station.minable = {
+    hardness = 0.2,
+    mining_time = 0.5,
+    result = "service_station"
+}
 service_station.logistics_radius = 10
 service_station.construction_radius = 0
+for _, layer in pairs(service_station.base.layers) do
+    if layer.filename == "__base__/graphics/entity/roboport/roboport-base.png" then
+        layer.filename = "__Constructron-Continued__/graphics/entity/constructotron-service-station.png"
+        layer.width = 114
+        layer.height = 139
+        layer.shift = layer.hr_version.shift
+        layer.hr_version.filename = "__Constructron-Continued__/graphics/entity/hr-constructotron-service-station.png"
+    end
+end
+-- Beacon Antenna
+local shft_1 = util.by_pixel(-1, -55)
+local shft_2 = util.by_pixel(100.5, 15.5)
+service_station.base_animation = {
+    layers = { -- Base
+    {
+        filename = "__Constructron-Continued__/graphics/entity/antenna.png",
+        width = 54,
+        height = 50,
+        line_length = 8,
+        frame_count = 32,
+        animation_speed = 0.5,
+        shift = {shft_1[1] - 0.5, shft_1[2] - 0.5}
+    }, -- Shadow
+    {
+        filename = "__Constructron-Continued__/graphics/entity/antenna-shadow.png",
+        width = 63,
+        height = 49,
+        line_length = 8,
+        frame_count = 32,
+        animation_speed = 0.5,
+        shift = {shft_2[1] - 0.5, shft_2[2] - 0.5},
+        draw_as_shadow = true
+    }}
+}
 
 local service_station_item = table.deepcopy(data.raw["item"]["roboport"])
 service_station_item.name = "service_station"
