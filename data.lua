835e38bc1958e299bb9297d8fb7398b51b8b41d6
Nicolas Lang <git@nicolas-lang.de>
2022-03-05 00:43:12 +0100

    moved pathing proxy to own file

--- data.lua
+++ data.lua
@@ -1,2 +1,3 @@
 require("__Constructron-Continued__.data.constructron")
+require("__Constructron-Continued__.data.constructron_pathing_proxy")
 require("__Constructron-Continued__.data.service_station")
