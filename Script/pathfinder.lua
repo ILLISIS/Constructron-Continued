efbb573e3012cbc7820f1d56ece20eaa392389b9
Nicolas Lang <git@nicolas-lang.de>
2022-03-05 11:42:59 +0100

    added some fallbacks to the pathfinding

--- Script/pathfinder.lua
+++ Script/pathfinder.lua
@@ -6,8 +6,8 @@ local me = {}
 
 me.init = function()
     global.pathfinder_requests = global.pathfinder_requests or {}
-    for key,request in pairs(global.pathfinder_requests) do
-        if ((game.tick -request.request_tick) > 60 *60 ) then -- one minute
+    for key, request in pairs(global.pathfinder_requests) do
+        if ((game.tick - request.request_tick) > 60 * 60) then -- one minute
             global.pathfinder_requests[key] = nil
         end
     end
@@ -30,47 +30,79 @@ me.clean_linear_path = function(path)
     return new_path
 end
 
-me.request_path = function(spidertrons, pathfinding_proxy_entity_name, goal)
+me.find_new_position = function(position, surface)
+    -- to avoid issues with pathing we prefer a unobstructed 6x6 area as pathing start/target
+    -- if that is unavaliable, we try to fall back to smaller alternatives and hope for the best
+    for _, size in pairs({6, 4, 2, 1}) do
+        local new_position = surface.find_non_colliding_position("constructron_pathing_proxy_" .. size, position, 32, 0.1, false)
+        if new_position then
+            return new_position
+        end
+        debug_lib.DebugLog("area of size " .. size .. " near " .. serpent.block(position) .. " is not reachable")
+    end
+end
+
+me.request_path = function(spidertrons, _, destination)
     local spidertron = spidertrons[1]
-    local new_start, new_goal
+    local new_start, new_destination
     if spidertron.valid then
         local surface = spidertron.surface
-        new_start = surface.find_non_colliding_position(pathfinding_proxy_entity_name, spidertron.position, 32, 0.1, false)
-        new_goal = surface.find_non_colliding_position(pathfinding_proxy_entity_name, goal, 32, 0.1, false)
-        debug_lib.VisualDebugCircle(goal, surface, color_lib.color_alpha(color_lib.colors.red, 0.2))
-        debug_lib.VisualDebugCircle(new_goal, surface, color_lib.color_alpha(color_lib.colors.green, 0.2))
-        if new_goal and new_start then
-            local pathing_collision_mask = {"water-tile",  "consider-tile-transitions", "colliding-with-tiles-only"}
-            if game.active_mods["space-exploration"] then
-                local spaceship_collision_layer = collision_mask_util_extended.get_named_collision_mask("moving-tile")
-                local empty_space_collision_layer = collision_mask_util_extended.get_named_collision_mask("empty-space-tile")
-                table.insert(pathing_collision_mask, spaceship_collision_layer)
-                table.insert(pathing_collision_mask, empty_space_collision_layer)
-            end
-            local request_id = surface.request_path {
-                bounding_box = {{-0.5, -0.5}, {0.5, 0.5}},
-                collision_mask = pathing_collision_mask,
-                start = new_start,
-                goal = new_goal,
-                force = spidertron.force,
-                pathfinding_flags = {
-                    cache = false,
-                    low_priority = true
-                }
-            }
-            global.pathfinder_requests[request_id] = {
-                spidertrons = spidertrons
-                ,request_tick = game.tick
-            }
-            for _, _spidertron in ipairs(spidertrons) do
-                _spidertron.autopilot_destination = nil
-            end
+
+        debug_lib.VisualDebugCircle(spidertron.position, surface, color_lib.color_alpha(color_lib.colors.red, 0.2), "S")
+        new_start = me.find_new_position(spidertron.position, surface)
+        if new_start then
+            debug_lib.VisualDebugCircle(new_start, surface, color_lib.color_alpha(color_lib.colors.green, 0.2), "S+")
         else
-            debug_lib.VisualDebugText("pathfinding request failed: start or target not reachable", spidertron)
-            return nil
+            debug_lib.VisualDebugText("pathing start position not reachable", spidertron)
+            -- try the initial position and hope for the best
+            new_start = spidertron.position
+        end
+
+        debug_lib.VisualDebugCircle(destination, surface, color_lib.color_alpha(color_lib.colors.red, 0.2), "T")
+        new_destination = me.find_new_position(destination, surface)
+        if new_destination then
+            debug_lib.VisualDebugCircle(new_destination, surface, color_lib.color_alpha(color_lib.colors.green, 0.2), "T+")
+        else
+            debug_lib.VisualDebugText("pathing destination position not reachable", spidertron)
+            -- try the initial position and hope for the best
+            new_destination = destination
+        end
+        debug_lib.VisualDebugCircle(destination, surface, color_lib.color_alpha(color_lib.colors.red, 0.2))
+
+        local pathing_collision_mask = {"water-tile", "consider-tile-transitions", "colliding-with-tiles-only"}
+        if game.active_mods["space-exploration"] then
+            local spaceship_collision_layer = collision_mask_util_extended.get_named_collision_mask("moving-tile")
+            local empty_space_collision_layer = collision_mask_util_extended.get_named_collision_mask("empty-space-tile")
+            table.insert(pathing_collision_mask, spaceship_collision_layer)
+            table.insert(pathing_collision_mask, empty_space_collision_layer)
+        end
+        local bounding_box_size = 0
+        local request_id =
+            surface.request_path {
+            bounding_box = {
+                {-bounding_box_size / 2, -bounding_box_size / 2},
+                {bounding_box_size / 2, bounding_box_size / 2}
+            },
+            collision_mask = pathing_collision_mask,
+            start = new_start,
+            goal = new_destination,
+            force = spidertron.force,
+            pathfinding_flags = {
+                cache = false,
+                low_priority = true
+            }
+        }
+        global.pathfinder_requests[request_id] = {
+            spidertrons = spidertrons,
+            request_tick = game.tick,
+            destination = destination,
+            bounding_box_size = bounding_box_size
+        }
+        for _, _spidertron in ipairs(spidertrons) do
+            _spidertron.autopilot_destination = nil
         end
     end
-    return new_goal
+    return new_destination
 end
 
 me.on_script_path_request_finished = function(event)
@@ -81,21 +113,25 @@ me.on_script_path_request_finished = function(event)
     local spidertrons = request.spidertrons
     local path = event.path
     if event.try_again_later then
+        -- todo reschedule after n ticks, global.pathfinder_requests should have all required information
         debug_lib.VisualDebugText("pathfinding failed: engine is busy", spidertrons[1])
-    elseif path then
-        local clean_path = me.clean_linear_path(path)
-        for _, spidertron in ipairs(spidertrons) do
-            spidertron.autopilot_destination = nil
-            local x = 0
-            for i, waypoint in ipairs(clean_path) do
-                spidertron.add_autopilot_destination(waypoint.position)
-                debug_lib.VisualDebugCircle(waypoint.position, spidertron.surface, color_lib.color_alpha(color_lib.colors.pink, 0.2), tostring(x))
-                x = x + 1
+        request.try_again_later = true
+    else
+        if path then
+            local clean_path = me.clean_linear_path(path)
+            for _, spidertron in ipairs(spidertrons) do
+                spidertron.autopilot_destination = nil
+                local x = 0
+                for i, waypoint in ipairs(clean_path) do
+                    spidertron.add_autopilot_destination(waypoint.position)
+                    debug_lib.VisualDebugCircle(waypoint.position, spidertron.surface, color_lib.color_alpha(color_lib.colors.pink, 0.2), tostring(x))
+                    x = x + 1
+                end
             end
+        else
+            debug_lib.VisualDebugText("pathfinder callback: path nil", spidertrons[1])
         end
         global.pathfinder_requests[event.id] = nil
-    elseif not path then
-        debug_lib.VisualDebugText("pathfinder callback: path nil", spidertrons[1])
     end
 end
 
