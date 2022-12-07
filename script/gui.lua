69e555efb44ba482bd65e24c8679e51fada80137
Florian Gebhardt <me@fgardt.dev>
2022-12-07 20:23:23 +0100

    gui builder as member of gui

--- script/gui.lua
+++ script/gui.lua
@@ -1,44 +1,44 @@
-local gui_builder = require("gui_builder")
 local mod_gui = require("mod-gui")
 
 local gui = {}
 
-gui.selected_surface = 1
+gui.builder = require("gui_builder")
 
-function gui.init()
-    for _, player in pairs(game.connected_players) do
-        local buttonFlow = mod_gui.get_button_flow(player)
+---@param player LuaPlayer
+function gui.init(player)
+    gui.selected_surface[player.index] = 1
 
-        -- recreate mod-gui button if it already exists
-        local flowButton = buttonFlow[gui_builder.flowButtonName]
-        if flowButton then
-            flowButton.destroy()
-        end
+    local buttonFlow = mod_gui.get_button_flow(player)
 
-        gui_builder.buildModGuiButton(buttonFlow)
+    -- recreate mod-gui button if it already exists
+    local flowButton = buttonFlow[gui.builder.flowButtonName]
+    if flowButton then
+        flowButton.destroy()
+    end
 
-        -- destroy mainFrame if it already exists
-        local mainFrame = player.gui.screen[gui_builder.mainFrameName]
-        if mainFrame then
-            mainFrame.destroy()
-        end
+    gui.builder.buildModGuiButton(buttonFlow)
 
-        -- destroy settingsFrame if it already exists
-        local preferencesFrame = player.gui.screen[gui_builder.preferencesFrameName]
-        if preferencesFrame then
-            preferencesFrame.destroy()
-        end
+    -- destroy mainFrame if it already exists
+    local mainFrame = player.gui.screen[gui.builder.mainFrameName]
+    if mainFrame then
+        mainFrame.destroy()
+    end
+
+    -- destroy settingsFrame if it already exists
+    local preferencesFrame = player.gui.screen[gui.builder.preferencesFrameName]
+    if preferencesFrame then
+        preferencesFrame.destroy()
     end
 end
 
 ---@param player LuaPlayer
 ---@param _ LuaGuiElement
 function gui.toggleMain(player, _)
-    local mainFrame = player.gui.screen[gui_builder.mainFrameName] --[[@as LuaGuiElement]]
-    local preferencesFrame = player.gui.screen[gui_builder.preferencesFrameName] --[[@as LuaGuiElement]]
+    local mainFrame = player.gui.screen[gui.builder.mainFrameName] --[[@as LuaGuiElement]]
+    local preferencesFrame = player.gui.screen[gui.builder.preferencesFrameName] --[[@as LuaGuiElement]]
 
     if not mainFrame then
-        gui_builder.buildMainGui(player)
+        gui.builder.buildMainGui(player)
     else
         if preferencesFrame and preferencesFrame.visible then
             preferencesFrame.visible = false
@@ -53,10 +53,10 @@ end
 ---@param player LuaPlayer
 ---@param _ LuaGuiElement
 function gui.togglePreferences(player, _)
-    local preferencesFrame = player.gui.screen[gui_builder.preferencesFrameName] --[[@as LuaGuiElement]]
+    local preferencesFrame = player.gui.screen[gui.builder.preferencesFrameName] --[[@as LuaGuiElement]]
 
     if not preferencesFrame then
-        gui_builder.buildPreferencesGui(player)
+        gui.builder.buildPreferencesGui(player)
     else
         preferencesFrame.visible = not preferencesFrame.visible
         preferencesFrame.bring_to_front()
