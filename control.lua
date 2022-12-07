563312863c59772fa974c30d6a9d994a908600c8
Florian Gebhardt <me@fgardt.dev>
2022-12-07 20:49:16 +0100

    init gui for new players

--- control.lua
+++ control.lua
@@ -83,6 +83,9 @@ script.on_event({ev.on_entity_destroyed, ev.script_raised_destroy}, ctron.on_ent
 
 script.on_event(ev.on_runtime_mod_setting_changed, ctron.mod_settings_changed)
 
+script.on_event(ev.on_player_created, (function(event) --[[@cast event EventData.on_player_created]]
+    gui.init(game.players[event.player_index])
+end))
 script.on_event(ev.on_player_changed_surface, gui_handler.player_changed_surfaces)
 
 gui_handler.register()
