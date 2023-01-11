31b0ece30c8193548bdec30efc3f4187a3ee04fa
Florian Gebhardt <me@fgardt.dev>
2023-01-11 19:14:01 +0100

    hide ctron remote in interfaces

--- data/constructron_remote.lua
+++ data/constructron_remote.lua
@@ -1,5 +1,5 @@
 local constructron_remote = table.deepcopy(data.raw["spidertron-remote"]["spidertron-remote"])
 constructron_remote.name = "constructron-remote"
-constructron_remote.flags = {"only-in-cursor", "not-stackable"}
+constructron_remote.flags = {"only-in-cursor", "not-stackable", "hidden", "hide-from-bonus-gui", "hide-from-fuel-tooltip"}
 
 data:extend({constructron_remote})
