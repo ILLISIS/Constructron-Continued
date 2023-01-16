de333f7893a446f32082b007c09268bb5a6a7446
Florian Gebhardt <me@fgardt.dev>
2023-01-16 07:22:54 +0100

    reduce amount of generated equipments to a minimum

--- data/roboports.lua
+++ data/roboports.lua
@@ -1,18 +1,37 @@
--- from ClosestFirstOneDotOne
-local roboports = {}
-for name, eq in pairs(data.raw["roboport-equipment"]) do
-   roboports[name] = eq
-end
-for _, eq in pairs(roboports) do
-   local radius = eq.construction_radius
-   for i=0, radius do
-      local robocopy = table.deepcopy(eq)
-      robocopy.construction_radius = i
-      robocopy.localised_name = {"equipment-name." .. robocopy.name}
-      robocopy.name = robocopy.name .. "-reduced-" .. i;
-      if not eq.take_result then
-         robocopy.take_result = eq.name
-      end
-      data:extend{robocopy};
+-- create copies of roboports with reduced construction radius
+local roboports = data.raw["roboport-equipment"]
+local reduced_roboports = {}
+
+for name, eq in pairs(roboports) do
+   local max_radius = math.min(eq.construction_radius, 1)
+
+   -- skip if equipment has no construction radius
+   if max_radius == 0 then
+      goto continue
    end
+
+   -- create copies with reduced construction radius
+   for i=0, max_radius do
+      local eq_copy = table.deepcopy(eq)
+
+      eq_copy.construction_radius = i
+      eq_copy.localised_name = {"equipment-name." .. name}
+      eq_copy.name = name .. "-reduced-" .. i;
+
+      if not eq.take_result then
+         eq_copy.take_result = name
+      end
+
+      table.insert(reduced_roboports, eq_copy)
+   end
+
+   ::continue::
 end
+
+data:extend(reduced_roboports)
+
+local fun_stuff = 43
+
+local fun_stufa = 42
+
+local fun_stufc = 41
