d0f6cf677a34e772132f99bd5cd2057cfdaf005f
ILLISIS <jayson.harris01@gmail.com>
2024-08-01 21:42:04 +1000

    stash

--- data.lua
+++ data.lua
@@ -3,7 +3,7 @@ require("data/constructron_pathing_proxy")
 require("data/service_station")
 require("data/station_combinator")
 require("data/selection_tool")
-require("data/shortcut")
+require("data/shortcuts")
 require("data/custom_input")
 require("data/styles")
 
