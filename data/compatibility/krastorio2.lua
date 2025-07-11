c6d6c3edfdadbcfd8946f2fc8d69bb5ed021d906
ILLISIS <jayson.harris01@gmail.com>
2025-07-12 00:09:31 +1000

    feat: Initial compatibility with Krastorio2 for 2.0.

--- data/compatibility/krastorio2.lua
+++ data/compatibility/krastorio2.lua
@@ -1,6 +1,10 @@
 if mods["Krastorio2"] then
   local spider = data.raw["spider-vehicle"]["constructron"]
-  if spider then
+  if data.raw["spider-vehicle"]["constructron"] then
     spider.equipment_grid = "kr-spidertron-equipment-grid"
+  end
+  local spider_rocket = data.raw["spider-vehicle"]["constructron-rocket-powered"]
+  if data.raw["spider-vehicle"]["constructron-rocket-powered"] then
+    spider_rocket.equipment_grid = "kr-spidertron-equipment-grid"
   end
 end
